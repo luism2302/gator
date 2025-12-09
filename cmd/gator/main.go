18b21662e4177f19844b9fa0c350f185752bfd8a
luism2302 <luis.urzua2302@gmail.com>
2025-12-08 20:15:45 -0600

    Completed login and fixed bug in writing to config

--- cmd/gator/main.go
+++ cmd/gator/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"github.com/luism2302/gator/internal/commands"
 	"github.com/luism2302/gator/internal/config"
 	"log"
+	"os"
 )
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+		log.Fatal(err)
 	}
-	err = cfg.SetUser("luis")
+	state := commands.State{
+		Cfg: &cfg,
+	}
+	cmds := commands.Commands{
+		MapHandler: make(map[string]func(*commands.State, commands.Command) error),
+	}
+	cmds.Register("login", commands.HandlerLogin)
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("error: no command provided. Usage: gator <command> [args]")
+	}
+	cmdName := args[1]
+	cmdArgs := []string{}
+	if len(args) > 2 {
+		cmdArgs = args[2:]
+	}
+	cmd := commands.Command{
+		Name: cmdName,
+		Args: cmdArgs,
+	}
+	err = cmds.Run(&state, cmd)
 	if err != nil {
-		log.Fatalf("error setting user: %v", err)
+		log.Fatal(err)
 	}
 	cfg, err = config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
+
+	fmt.Println(cfg)
 }
